cbb1a30cefcd1537a5797192f2555db78442036c
Thomas <traianmail@gmail.com>
2023-04-17 12:18:03 +0300

    leaving

--- question3.R
+++ question3.R
@@ -1,3 +1,4 @@
+set.seed(123)
 seeds_orig = read.csv('seeds_dataset.csv', header=T)
 
 rm_cols = which(colnames(seeds_orig) %in% c('seeID', 'seedType'))
@@ -13,4 +14,5 @@ scseeds = as.data.frame(scseeds) #convert it to data frame
 print(sapply(scseeds, mean)) #indeed the mean is 0, (very close to zero due to rounding errors) for every attribute 
 print(sapply(scseeds, sd)) #indeed the standard deviation is 1 for every attribute
 
+c = kmeans(scseeds, 3, nstart=25) #apply kmeans algorithm to scseeds with 3 centers and nstart 25!!!!!!!!!!!!!!! 
 
