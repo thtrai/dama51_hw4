255e9f7152556b279df5a615621b35f293d09bad
trai <traianmail@gmail.com>
2023-04-21 19:24:03 +0300

    working on topic 3 with seed

--- topic3.R
+++ topic3.R
@@ -1,6 +1,8 @@
+#import seeds data and run set.seed as stated in topic
 set.seed(123)
 seeds_orig = read.csv('seeds_dataset.csv', header=T)
 
+#find the column numbers of seeID,seedType to remove them
 rm_cols = which(colnames(seeds_orig) %in% c('seeID', 'seedType'))
 print(rm_cols)
 
@@ -16,3 +18,12 @@ print(sapply(scseeds, sd)) #indeed the standard deviation is 1 for every attribu
 
 c = kmeans(scseeds, 3, nstart=25) #apply kmeans algorithm to scseeds with 3 centers and nstart 25!!!!!!!!!!!!!!! 
 
+#print centroids perimeter, rounded 
+print(round(c$centers[,'perimeter'],3))
+
+#print the clusters which belong the data of rows 9,55,189 respectively
+print(c$cluster[c(9,55,189)])
+
+#print the euclidean distance of the centroids, rounded
+
+print(round(dist(c$centers, method='euclidean'),3))
