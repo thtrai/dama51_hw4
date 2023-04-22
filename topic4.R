0e7712c6d40f10b15a7f0f593ac99f018f3e9a39
trai <traianmail@gmail.com>
2023-04-23 02:21:04 +0300

    working on 4d

--- topic4.R
+++ topic4.R
@@ -22,5 +22,53 @@ europe = europe[,-1]
 #topic 4b
 
 #calculate euclidean distance matrix of Greece, Belgium, Spain and Finland, rounded to 3 decimals
+distMatrix = round(dist(europe[c('Greece','Spain','Belgium','Finland'),], method='euclidean'),3)
 
-print(round(dist(europe[c('Greece','Spain','Belgium','Finland'),], method='euclidean'),3))
+print(distMatrix)
+
+#topic 4c
+library(cluster) #must be installed. Contains agnes() function
+
+#distance matrix of europe with euclidean metric
+distMatrixFull = dist(europe, method='euclidean')
+
+#since the distance matrix has been created with euclidean metric,
+#I don't have to define the metric in agnes() function
+
+#agglomarative cluster using distance matrix, with complete clustering method
+cluster_complete = agnes(distMatrixFull, method='complete')
+
+#agglomarative cluster using distance matrix, with single clustering method
+cluster_single = agnes(distMatrixFull, method='single')
+
+pltree(cluster_complete)
+pltree(cluster_single)
+
+#topic 4d
+#create the vector with the 7 group memberships as g, from the 
+#previous complete linkage hierarchical clustering
+
+g = cutree(cluster_complete, k=7)
+
+#extract the countries names from distMatrixFull
+countries = rownames(as.matrix(distMatrixFull))
+
+#find the index numbers of the countries
+index_switz = which(countries == 'Switzerland')
+index_norway = which(countries == 'Norway')
+
+#find in which cluster the countries has been assigned
+cluster_switz = g[index_switz]
+cluster_norway = g[index_norway]
+
+#find the indeces of countries with the same group as Switzerland and Norway
+index_group_switz = which(g==cluster_switz)
+index_group_norway = which(g==cluster_norway)
+
+#Group with Switzerland
+group1 = countries[index_group_switz]
+print(group1)
+
+#Group with Norway
+group2 = countries[index_group_norway]
+print(group2)
