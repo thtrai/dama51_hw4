7a07b416b3b2fcfdb1c0c73ddabb64936d145956
trai <traianmail@gmail.com>
2023-04-24 04:17:08 +0300

    working

--- topic5.R
+++ topic5.R
@@ -12,5 +12,20 @@ print(summary(appstrans))
 #topcic 5c
 itemFrequencyPlot(appstrans, topN=10)
 
+#topcic 5d
+itemsets = apriori(appstrans, parameter=list(support=0.25, confidence=0.8, minlen=2))
 
+itemsets_by_sup = sort(itemsets, by = 'support')
+
+inspect(head(itemsets_by_sup,4))
+
+print(summary(itemsets))
+
+
+#topic 5e
+#create vector where true is for the rules where "Instagram" in on rhs
+insta_vector = rhs(itemsets) %in% 'Instagram'
+
+#print the lhs of these rules
+inspect(lhs(itemsets)[insta_vector])
 
