cf31e900934835390150855a48948f126f2ed4ce
thtrai <traianmail@gmail.com>
2023-04-25 23:29:12 +0300

    finished. Need review and script run

--- topic5.R
+++ topic5.R
@@ -14,24 +14,24 @@ itemFrequencyPlot(appstrans, topN=10)
 
 #topcic 5d
 #run apriori with the given parameters.
-itemsets = apriori(appstrans, parameter=list(support=0.25, confidence=0.8, minlen=2))
+rules = apriori(appstrans, parameter=list(support=0.25, confidence=0.8, minlen=2))
 
 #sort by support
-itemsets_by_sup = sort(itemsets, by = 'support')
+rules_by_sup = sort(rules, by = 'support')
 
-inspect(head(itemsets_by_sup,4))
+inspect(head(rules_by_sup,4))
 
-print(summary(itemsets))
+print(summary(rules))
 
 
 #topic 5e
 #create vector where true is for the rules where "Instagram" in on rhs
-insta_vector = rhs(itemsets) %in% 'Instagram'
+insta_vector = rhs(rules) %in% 'Instagram'
 
 #print the lhs of these rules
-inspect(lhs(itemsets)[insta_vector])
+inspect(lhs(rules)[insta_vector])
 
 library(arulesViz)
-plot(itemsets, method='paracoord')
+plot(rules, method='paracoord')
 
 
